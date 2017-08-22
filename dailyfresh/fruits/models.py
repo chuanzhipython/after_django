b6e45f426d44d31b49a9824aa8c8b7f6853cfe75
chuanzhipython <869858978@qq.com>
2017-08-22 20:30:44 +0800

    删除str方法

--- dailyfresh/fruits/models.py
+++ dailyfresh/fruits/models.py
@@ -7,5 +7,5 @@ class Student(models.Model):
     sname = models.CharField(max_length=20)
     snum = models.IntegerField(11)
 
-    def __str__(self):
-        return self.sname
+    # def __str__(self):
+    #     return self.sname
