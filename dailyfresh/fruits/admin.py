564d0f53925cf248c95c68eabf637a5a07791a6b
chuanzhipython <869858978@qq.com>
2017-08-22 21:19:33 +0800

    fix py

--- dailyfresh/fruits/admin.py
+++ dailyfresh/fruits/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Student
 
 # Register your models here.
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['sname', 'snum']
+
+admin.site.register(Student)
